664700bf48748f46c824ecbc456fbcaa43e75c78
gil7788 <gil7788@gmail.com>
2025-08-20 21:02:33 +0300

    Nested Dispatching

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ mod system;
 mod types;
 
 use crate::{
-	support::{Dispatch, DispatchResult, Extrinsic},
-	types::{BalancesPallet, Runtime, RuntimeCall, SystemConfig, SystemPallet, types::Block},
+	support::{Dispatch, DispatchResult, Extrinsic, Header},
+	types::{types::Block, BalancesPallet, Runtime, RuntimeCall, SystemConfig, SystemPallet},
 };
 
 impl Runtime {
@@ -56,23 +56,47 @@ impl crate::support::Dispatch for Runtime {
 
 fn main() {
 	println!("Hello, world!");
+	
 	let mut run_time = Runtime::new();
 	let alice: String = "alice".to_string();
 	let bob: String = "bob".to_string();
 	let charlie: String = "charlie".to_string();
+
 	run_time.balances.set_balance(&alice, 100);
-	run_time.system.inc_block_number();
-	assert_eq!(run_time.system.block_number(), 1);
+	// run_time.system.inc_block_number();
+	// assert_eq!(run_time.system.block_number(), 1);
 
-	run_time.system.inc_nonce(&alice);
-	let _res = run_time.balances.transfer(&alice, &bob, 30).map_err(|err| eprintln!("{}", err));
-	run_time.system.inc_nonce(&alice);
+	// run_time.system.inc_nonce(&alice);
+	// let _res = run_time.balances.transfer(&alice, &bob, 30).map_err(|err| eprintln!("{}", err));
+	// run_time.system.inc_nonce(&alice);
 
-	let _res = run_time
-		.balances
-		.transfer(&alice, &charlie, 20)
-		.map_err(|err| eprintln!("{}", err));
-	run_time.system.inc_nonce(&alice);
+	// let _res = run_time
+	// 	.balances
+	// 	.transfer(&alice, &charlie, 20)
+	// 	.map_err(|err| eprintln!("{}", err));
+	// run_time.system.inc_nonce(&alice);
+
+	let block =  Block {
+		header: Header { block_number: 1 },
+		extrinsics: {
+			vec![
+				Extrinsic {
+					caller: alice.clone(),
+					call: RuntimeCall::BalancesTransfer { to: bob.clone(), amount: 69 }
+				},
+				Extrinsic {
+					caller: alice.clone(),
+					call: RuntimeCall::BalancesTransfer { to: charlie, amount: 12 }
+				},
+				Extrinsic {
+					caller: alice.clone(),
+					call: RuntimeCall::BalancesTransfer { to: bob, amount: 11 }
+				}
+			]
+		}
+	};
+
+	run_time.execute_block(block).expect("invalid block");
 
 	println!("{:#?}", run_time);
 }
